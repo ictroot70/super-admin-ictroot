b0ea3299637643da761e3ac802c9e2212af7a0bd
Dmitry Muzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2026-04-26 11:10:45 +0300

    SCRUM-276 feat: update user tab imports to use consistent naming for Followers and Following tabs

--- app/(super-admin)/users/[userId]/[tab]/page.tsx
+++ app/(super-admin)/users/[userId]/[tab]/page.tsx
@@ -3,14 +3,19 @@ import type { ComponentType } from 'react'
 import { notFound } from 'next/navigation'
 
 import { isUserTab, type UserTab } from '@/features/admin/user-details/model'
-import { PaymentsTab, Followers, Following, UploadedPhotos } from '@/features/admin/user-details/ui'
+import {
+  PaymentsTab,
+  FollowersTab,
+  FollowingTab,
+  UploadedPhotos,
+} from '@/features/admin/user-details/ui'
 import { parseUserIdParam } from '@/shared/lib/route-params'
 
 const TAB_TO_COMPONENT_MAP: Record<UserTab, ComponentType> = {
   'uploaded-photos': UploadedPhotos,
   payments: PaymentsTab,
-  followers: Followers,
-  following: Following,
+  followers: FollowersTab,
+  following: FollowingTab,
 }
 
 type Props = {
--- features/admin/user-details/ui/index.ts
+++ features/admin/user-details/ui/index.ts
@@ -1,4 +1,4 @@
 export * from './uploaded-photos-tab'
 export * from './payments-tab/payments-tab'
-export * from './followers-tab'
-export * from './following-tab'
+export * from './followers-tab/followers-tab'
+export * from './following-tab/following-tab'
