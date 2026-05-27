d0b2de7c3754424a011396f6238dbce899c3c329
Sem Antony <smbat1584@gmail.com>
2026-05-27 13:24:51 +0400

    Hotfix/image size and config (#22)
    * fix hostname in next.config.ts and apollo client link
    
    * fix image: update image size and config for better display

--- shared/constant/imageSizes.ts
+++ shared/constant/imageSizes.ts
@@ -1,19 +1,24 @@
 import type { ImageProps } from 'next/image'
 
-type ImageLoadingStrategy = Pick<ImageProps, 'fetchPriority' | 'loading' | 'priority'>
+type ImageLoadingStrategy = Pick<ImageProps, 'fetchPriority' | 'loading' | 'priority' | 'quality'>
 
 export const IMAGE_SIZES = {
   PUBLIC_POST:
-    '(max-width: 640px) 100vw, (max-width: 968px) calc(50vw - 12px), (max-width: 1200px) calc(33vw - 12px), 234px',
+    '(min-width: 1280px) calc(25vw - 12px), (min-width: 1024px) calc(33vw - 12px), (min-width: 640px) calc(50vw - 12px), 100vw',
   POST_CARD:
-    '(max-width: 640px) 100vw, (max-width: 968px) calc(50vw - 12px), (max-width: 1200px) calc(33vw - 12px), 234px',
+    '(min-width: 1280px) calc(25vw - 12px), (min-width: 1024px) calc(33vw - 12px), (min-width: 640px) calc(50vw - 12px), 100vw',
   POST_MODAL: '(max-width: 968px) 95vw, 490px',
 } as const
 
 export const IMAGE_LOADING_STRATEGY = {
-  lcp: { priority: true, fetchPriority: 'high', loading: 'eager' },
-  default: { priority: false, fetchPriority: 'auto', loading: 'lazy' },
-  low: { priority: false, fetchPriority: 'low', loading: 'lazy' },
+  lcp: { priority: true, fetchPriority: 'high' as const, loading: 'eager' as const, quality: 75 },
+  default: {
+    priority: false,
+    fetchPriority: 'auto' as const,
+    loading: 'lazy' as const,
+    quality: 60,
+  },
+  low: { priority: false, fetchPriority: 'low' as const, loading: 'lazy' as const, quality: 50 },
 } satisfies Record<'default' | 'lcp' | 'low', ImageLoadingStrategy>
 
 export const getAvatarImageSizes = (size: number): string => `${size}px`
