08f1ee910817c385a3be8d282c6da82dc9dc136f
Smbat1505 <smbat1584@gmail.com>
2026-05-27 13:20:52 +0400

    fix image: update image size and config for better display

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
+    minimumCacheTTL: 60 * 60 * 24 * 7,
+    qualities: [60, 75],
     remotePatterns: [
       {
         protocol: 'https',
